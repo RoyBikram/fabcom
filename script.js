c9cd8c1dc0cf72ef8193c47291a9155cdcc22cac
Bikram Roy <bikramroy364@gmail.com>
2021-10-22 13:06:09 +0530

    add active member section

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function scrollHeader(){
     if(this.scrollY >= 50) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+// ---------Swiper----------//
+const myswiper = document.querySelector('.mySwiper')
+var swiper = new Swiper(myswiper, {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 40,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
