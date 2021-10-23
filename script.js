adc77884eb2a0ed8b83b18a5cbe5389066f60b0a
Bikram Roy <bikramroy364@gmail.com>
2021-10-23 09:02:57 +0530

    Fix smooth scrolling position using new method & add active nav link feature

--- script.js
+++ script.js
@@ -17,4 +17,50 @@ var swiper = new Swiper(myswiper, {
         delay: 2500,
         disableOnInteraction: false,
       },
-  });
+});
+  
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 200;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[data-location*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav_menu a[data-location*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+function scrollToTargetAdjusted(element) {
+    if (element.id == 'community' || element.id == 'member' || element.id == 'app') {
+        var headerOffset = 120;
+        console.log('working');
+    } else {
+        var headerOffset = 50;
+    }
+    var elementPosition = element.offsetTop;
+  var offsetPosition = elementPosition - headerOffset;
+  
+  window.scroll({
+      top: offsetPosition,
+      behavior: "smooth"
+  });   
+}
+// ---------------Smooth Scroll----------------
+document.querySelectorAll('.nav_menu a').forEach(each => {
+    each.addEventListener('click', (event) => {
+        var location = event.target.getAttribute('data-location')
+        scrollToTargetAdjusted(document.querySelector(location))
+        
+    })
+});
