ccdcf051f41c29b06d927fa842b90d8828debe9e
Bikram Roy <bikramroy364@gmail.com>
2021-10-27 11:06:34 +0530

    Add animation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+// -------------------AOS animation initialize------------------
+AOS.init();
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader(){
     const nav = document.querySelector('header')
     if(this.scrollY >= 50) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
